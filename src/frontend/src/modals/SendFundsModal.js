7c383e32a04fb25b4a862523f370347e1af34af7
nikolas-con <nikos.a.constantinou@gmail.com>
2022-12-19 16:06:57 +0200

    format

--- src/frontend/src/modals/SendFundsModal.js
+++ src/frontend/src/modals/SendFundsModal.js
@@ -1,25 +1,55 @@
-import { useState } from 'react';
-import { useToast,Modal, ModalBody, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, Flex, Input, ModalFooter, Button } from '@chakra-ui/react';
+import { useState } from "react";
+import {
+  useToast,
+  Modal,
+  ModalBody,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  Flex,
+  Input,
+  ModalFooter,
+  Button,
+} from "@chakra-ui/react";
 import { ethers } from "ethers";
 
-const SendFundsModal = ({ provider, network, setTransactions, setBalance, setCycles, setWaiting, actor, address, onClose, isOpen }) => {
+const SendFundsModal = ({
+  provider,
+  network,
+  setTransactions,
+  setBalance,
+  setCycles,
+  setWaiting,
+  actor,
+  address,
+  onClose,
+  isOpen,
+}) => {
   const [amount, setAmount] = useState("");
   const [destination, setDestination] = useState("");
-  const toast = useToast()
+  const toast = useToast();
 
   const handleSignTx = async (e) => {
     e.preventDefault();
 
-    onClose()
+    onClose();
 
-    setWaiting(true)
+    setWaiting(true);
 
-    const nonce = await provider.getTransactionCount(address)
-    const gasPrice = await provider.getGasPrice().then((s) => s.toHexString())
-    const value = ethers.utils.parseEther(amount).toHexString()
-    const data = "0x00"
-    const gasLimit = ethers.BigNumber.from("24000").toHexString()
-    const transaction = { nonce, gasPrice, gasLimit, to: destination, value, data, };
+    const nonce = await provider.getTransactionCount(address);
+    const gasPrice = await provider.getGasPrice().then((s) => s.toHexString());
+    const value = ethers.utils.parseEther(amount).toHexString();
+    const data = "0x00";
+    const gasLimit = ethers.BigNumber.from("24000").toHexString();
+    const transaction = {
+      nonce,
+      gasPrice,
+      gasLimit,
+      to: destination,
+      value,
+      data,
+    };
 
     const serializeTx = Buffer.from(
       ethers.utils.serializeTransaction(transaction).slice(2) + "808080",
@@ -28,11 +58,20 @@ const SendFundsModal = ({ provider, network, setTransactions, setBalance, setCyc
 
     toast({ title: "Signing transaction...", variant: "subtle" });
 
-    const res = await actor.sign_evm_tx([...serializeTx], Number(network.chainId));
+    const res = await actor.sign_evm_tx(
+      [...serializeTx],
+      Number(network.chainId)
+    );
     if (res.Err) {
       const message = res.Err ?? "";
-      toast({ title: "Error", description: message, status: "error", variant: "subtle" });
-      return
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        variant: "subtle",
+      });
+      setWaiting(false);
+      return;
     }
     const signedTx = Buffer.from(res.Ok.sign_tx, "hex");
 
@@ -45,14 +84,16 @@ const SendFundsModal = ({ provider, network, setTransactions, setBalance, setCyc
     await provider.waitForTransaction(hash);
     toast({ title: `Transfered ${amount} ${network.nativeCurrency.symbol}` });
 
-    setWaiting(false)
+    setWaiting(false);
 
     const balance = await provider.getBalance(address);
     setBalance(ethers.utils.formatEther(balance));
 
-    setTransactions((txs) => [...txs, { data: signedTx, timestamp: new Date() }]);
-    // console.log(res)
-    setCycles(e => e - res.Ok.sign_cycles);
+    setTransactions((txs) => [
+      ...txs,
+      { data: signedTx, timestamp: new Date() },
+    ]);
+    setCycles((e) => e - res.Ok.sign_cycles);
   };
 
   return (
@@ -63,17 +104,30 @@ const SendFundsModal = ({ provider, network, setTransactions, setBalance, setCyc
         <ModalCloseButton />
         <ModalBody>
           <Flex>
-            <Input onChange={e => setDestination(e.target.value)} placeholder="Destination (Address)" />
-            <Input onChange={e => setAmount(e.target.value)} placeholder="Amount" type="number" ml="10px" width="120px" />
+            <Input
+              onChange={(e) => setDestination(e.target.value)}
+              placeholder="Destination (Address)"
+            />
+            <Input
+              onChange={(e) => setAmount(e.target.value)}
+              placeholder="Amount"
+              type="number"
+              ml="10px"
+              width="120px"
+            />
           </Flex>
         </ModalBody>
         <ModalFooter>
-          <Button variant='ghost' mr={3} onClick={onClose}>Close</Button>
-          <Button onClick={handleSignTx} disabled={!amount || amount === '0'}>Send</Button>
+          <Button variant="ghost" mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button onClick={handleSignTx} disabled={!amount || amount === "0"}>
+            Send
+          </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
-  )
-}
+  );
+};
 
 export default SendFundsModal;
--- src/frontend/src/modals/TopupModal.js
+++ src/frontend/src/modals/TopupModal.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import {
+  Modal,
+  ModalBody,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalFooter,
+} from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Link,
+  Text,
+  Box,
+  IconButton,
+  useToast,
+} from "@chakra-ui/react";
 
-import { useState } from 'react';
-import { Modal, ModalBody, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalFooter } from '@chakra-ui/react';
-import { Flex, Button, Link, Text, Box, IconButton, useToast } from '@chakra-ui/react';
-
-import { HiOutlineClipboardDocument, HiOutlineClipboardDocumentCheck } from "react-icons/hi2";
+import {
+  HiOutlineClipboardDocument,
+  HiOutlineClipboardDocumentCheck,
+} from "react-icons/hi2";
 
 import { getAccountId } from "../helpers/account";
 import { BACKEND_CANISTER_ID } from "../helpers/config";
@@ -11,25 +29,30 @@ import { BACKEND_CANISTER_ID } from "../helpers/config";
 import { getHostFromUrl } from "../helpers/utils";
 import { IC_URL } from "../helpers/config";
 
-const docsLink = 'https://github.com/dfinity/portal/blob/master/docs/developer-docs/integrations/t-ecdsa/t-ecdsa-how-it-works.md'
+const docsLink =
+  "https://github.com/dfinity/portal/blob/master/docs/developer-docs/integrations/t-ecdsa/t-ecdsa-how-it-works.md";
 
 const isLocal = getHostFromUrl(IC_URL).startsWith("localhost");
 
-const TopupModal = ({ actor, caller, setCycles, setWaiting, onClose, isOpen }) => {
-
+const TopupModal = ({
+  actor,
+  caller,
+  setCycles,
+  setWaiting,
+  onClose,
+  isOpen,
+}) => {
   const toast = useToast();
   const [hasCopied, setHasCopied] = useState(false);
 
   const topupCycles = async () => {
     toast({ title: "Converting balance to cycles...", variant: "subtle" });
-
-    setWaiting(true)
-
+    setWaiting(true);
     const res = await actor.convert_to_cycles();
 
-    toast({ title: "Balance converted successfully" });
+    setWaiting(false);
 
-    setWaiting(false)
+    toast({ title: "Balance converted successfully" });
 
     const _cycles = res.Ok;
     setCycles(_cycles);
@@ -55,9 +78,16 @@ const TopupModal = ({ actor, caller, setCycles, setWaiting, onClose, isOpen }) =
         <ModalBody>
           <Flex flexDir="column">
             <Text mb="12px">
-              Signing transactions requires payment in cycles. Topup by sending ICP to the account bellow.
+              Signing transactions requires payment in cycles. Topup by sending
+              ICP to the account bellow.
             </Text>
-            <Box mb="12px" p="8px" bgColor="ButtonText" color="white" borderRadius="md">
+            <Box
+              mb="12px"
+              p="8px"
+              bgColor="ButtonText"
+              color="white"
+              borderRadius="md"
+            >
               {subAccount}
               <IconButton
                 onClick={copyToClipboard}
@@ -75,21 +105,36 @@ const TopupModal = ({ actor, caller, setCycles, setWaiting, onClose, isOpen }) =
                 }
               />
             </Box>
-            {isLocal && <Text fontWeight="bold" mb="8px">
-              No need to topup with ICP in development.
-            </Text>}
+            {isLocal && (
+              <Text fontWeight="bold" mb="8px">
+                No need to topup with ICP in development.
+              </Text>
+            )}
             <Text>
-              For more read on <Link href={docsLink} isExternal>dfinity/portal</Link>.
+              For more read on{" "}
+              <Link href={docsLink} isExternal>
+                dfinity/portal
+              </Link>
+              .
             </Text>
           </Flex>
         </ModalBody>
         <ModalFooter>
-          <Button variant='ghost' mr={3} onClick={onClose}>Close</Button>
-          <Button onClick={() => {topupCycles(); onClose()}}>Notify</Button>
+          <Button variant="ghost" mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button
+            onClick={() => {
+              topupCycles();
+              onClose();
+            }}
+          >
+            Notify
+          </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
-  )
-}
+  );
+};
 
 export default TopupModal;
